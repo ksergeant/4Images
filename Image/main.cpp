4aa0b33b879f62b82a46656142b095acd3856494
Kevin <Kevin@macbook-air-de-kevin.home>
2016-08-18 23:16:55 +0200

    Initial Commit

---
+++ Image/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Image
+//
+//  Created by Kevin on 18/08/2016.
+//  Copyright © 2016 Kevin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
